1098705b6e8e2dc156f93f4dfde1511a3b7e4d6b
Evelyn Sprague <eospra59>
2020-01-28 11:08:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ from tkinter import * # import Tkinter library
 window = Tk()         # Create the application window
 
 # Add a label with the text "Hello"
-lbl = Label(window, text="Hello")
+lbl = Label(window, text="Hello, prepare to be processed. Please, do not resist.")
 
 # Place the label in the window at 0, 0
 lbl.grid(column=0, row=0)
-
 
 window.mainloop()     # Keep the window open
